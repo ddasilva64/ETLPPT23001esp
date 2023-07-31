925ceb939f0d7d1275eadf2020592588e139b0e5
Daniel da Silva <daniel.da.silva.jarque@gmail.com>
2023-07-31 22:34:12 +0200

    Project commit

---
+++ projects/PROWPI002/dwh/ISS_Customer_Product_view.sql
@@ -0,0 +1,16 @@
+CREATE VIEW target."ISS_Customer_Product_view"  AS 
+	SELECT
+       c."CustomerKey", c."GeographyKey", s."CurrencyKey", s."SalesTerritoryKey", c."Title", c."FirstName", 
+	   c."MiddleName", c."LastName", s."OrderDate", 
+	   target."NetIncomeFunc"(s."ExtendedAmount",s."ProductStandardCost",s."TaxAmt",s."Freight") "NetIncome",
+	   10.00 "CAC",
+	   "BirthDate", DATE_PART('year',AGE("BirthDate")) "Age", 
+	   "MaritalStatus","Suffix","Gender","TotalChildren", "NumberChildrenAtHome", 
+	   "Education","HouseOwnerFlag","NumberCarsOwned", "AddressLine1","AddressLine2", 
+	   "Phone","DateFirstPurchase","CommuteDistance", "Occupation",
+       p."ProductKey",p."ProductName", p."ProductSubcategoryKey"
+ 	FROM target."DimCustomer" c 
+      LEFT JOIN target."ISS_view" s
+	  ON s."CustomerKey" = c."CustomerKey" 
+	  LEFT JOIN target."DimProduct" p
+	  ON s."ProductKey" = p."ProductKey"
---
+++ projects/docs/ux_data_model.md
@@ -0,0 +1,22 @@
+## Data model in BSC  
+
+**_Notice!_**: We are delighted to announce significant advancements in our project. The dimensions have been successfully reduced to their minimum expression, and we have seamlessly integrated the **_PROWPI001_** indicators. Our approach has been strictly focused on achieving the desired indicators.
+
+<p><br></p>
+
+![BSC data model](https://i.imgur.com/ssmdsgu.png)  
+_BSC data model_
+
+<p><br></p> 
+
+[Disclaimer](../DISCLAIMER.md)
+
+<p><br></p> 
+
+[ChatGPT usage](../CHATGPT_USAGE.md)  
+
+<p><br></p>
+
+[UX:arrow_up:](ux.md)  
+
+[Back to Table of contents :arrow_double_up:](../README.md)
